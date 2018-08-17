76516774b47c3b9d648b2933f96df5f5ea75b4d8
Your Name <you@example.com>
2018-08-17 12:05:59 +0700

    sua restaurant

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -35,13 +35,13 @@ class RestaurantController extends Controller
         $district = District::all();
         $ward = Ward::all();
 
-        return view('restaurant.create', compact('category','provind','district','ward'));
+        return view('restaurant.create', compact('category', 'provind', 'district', 'ward'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -54,33 +54,62 @@ class RestaurantController extends Controller
                 'openTime' => 'required',
                 'closeTime' => 'required',
                 'shortDescription' => 'required',
+                'description' => 'required',
+            ],
+            [
+                'name.required' => 'Bạn chưa nhập tên nhà hàng',
+                'avartar.required' => 'Bạn chưa thêm ảnh',
+                'phone.required' => 'Bạn chưa nhập số điện thoại',
+                'openTime.required' => 'Bạn chưa nhập giờ mở cửa',
+                'closeTime.required' => 'Bạn chưa nhập giờ đóng cửa',
+                'shortDescription.required' => 'Bạn chưa nhập mô tả ngắn',
+                'description.required' => 'Bạn chưa nhập mô tả chi tiết',
             ]
 
         );
+        if($request->hasFile('avartar')){
+            echo "Đã có file";
+            $this->validate($request,
+                [
+                    'avartar' => 'mimes:jpg,jpeg,png,gif|max:2048',
+                ],
+                [
+                    'avartar.mimes' => 'Chỉ chấp nhận ảnh với đuôi .jpg .jpeg .png .gif',
+                    'avartar.max' => 'Ảnh giới hạn dung lượng không quá 2M',
+                ]
+            );
+            $avartar = $request->file('avartar');
+            $getAvartar = time().'_'.$avartar->getClientOriginalName();
+            $distional_path = public_path('/images');
+            $avartar->move($distional_path,$getAvartar);
+        }
+        else{
+            echo "chưa có file";
+        }
         $restaurant = new Restaurant();
-        $restaurant -> categoryID = Input::get('category');
-        $restaurant -> name = Input::get('name');
-        $restaurant -> avartar = Input::get('avartar');
+        $restaurant->categoryID = Input::get('category');
+        $restaurant->name = Input::get('name');
+        $restaurant->avartar = Input::get('avartar');
         $address = new Address();
-        $address -> provindID = Input::get('provind');
-        $address -> districtID = Input::get('district');
-        $address -> wardID = Input::get('ward');
-        $address -> save();
-        $restaurant -> addressID = $address->id;
-        $restaurant -> phone = Input::get('phone');
-        $restaurant -> openTime = Input::get('openTime');
-        $restaurant -> closeTime = Input::get('closeTime');
-        $restaurant -> shortDescription = Input::get('shortDescription');
-        $restaurant -> description = Input::get('description');
-        $restaurant -> status = Input::get('status');
-        $restaurant ->save();
+        $address->provindID = Input::get('provind');
+        $address->districtID = Input::get('district');
+        $address->wardID = Input::get('ward');
+        $address->save();
+        $restaurant->addressID = $address->id;
+        $restaurant->phone = Input::get('phone');
+        $restaurant->openTime = Input::get('openTime');
+        $restaurant->closeTime = Input::get('closeTime');
+        $restaurant->shortDescription = Input::get('shortDescription');
+        $restaurant->description = Input::get('description');
+        $restaurant->status = Input::get('status');
+        $restaurant->save();
         return 'OK';
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -91,7 +120,7 @@ class RestaurantController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -102,8 +131,8 @@ class RestaurantController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -114,7 +143,7 @@ class RestaurantController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
